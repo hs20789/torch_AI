647008515c359d4b48eed68992d6eacd28c9d20a
HeonSu <hs20789@outlook.com>
2026-07-26 15:21:14 +0900

    ch03 - 1

--- chapters/ch3/01_advanced_cv.py
+++ chapters/ch3/01_advanced_cv.py
@@ -1,7 +1,30 @@
+'''
+합성곱으로 이미지에서 특징을 감지하는 방법을 배우고, 이런 특징을 기반으로 이미지를 분류하는 방법을 자세히 알아본다.
+이후에는 이미지 증식을 통해 풍부한 특징을 얻고, 전이 학습으로 다른 사람이 만든 기존의 특징을 사용해 본다.
+그 다음 드롭아웃으로 모델을 최적화하는 방법을 간단히 알아본다.
+'''
+# %%
 import torch.nn as nn
+import torch.optim as optim
 import torch.nn.functional as F
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+# %%
+transform = transforms.Compose([transforms.ToTensor()])
+
+train_dataset = datasets.FashionMNIST(
+    root="./data", train=True, download=True, transform=transform
+)
+test_dataset = datasets.FashionMNIST(
+    root="./data", train=False, download=True, transform=transform
+)
+
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
 
+# %%
 class FashionMNISTModel(nn.Module):
     def __init__(self):
         super(FashionMNISTModel, self).__init__()
@@ -12,3 +35,64 @@ class FashionMNISTModel(nn.Module):
             nn.Linear(128, 10),
             nn.ReLU(),
         )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+model = FashionMNISTModel()
+
+loss_function = nn.NLLLoss()
+optimizer = optim.Adam(model.parameters())
+
+class FashionCNN(nn.Module):
+    def __init__(self):
+        super(FashionCNN, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+
+        self.fc1 = nn.Linear(64 * 6 * 6, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+
+    for batch, (X, y) in enumerate(dataloader):
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"손실: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+
+epochs = 5
+for t in range(epochs):
+    print(f"에폭 {t+1}\n-------------------------------")
+    train(train_loader, model, loss_function, optimizer)
+print("완료")
+
