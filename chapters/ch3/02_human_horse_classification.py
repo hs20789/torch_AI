57b474aa9c3592f3d89dab213d55414d28c9d3c1
HeonSu <hs20789@outlook.com>
2026-07-27 23:43:58 +0900

    ch03-4

--- chapters/ch3/02_human_horse_classification.py
+++ chapters/ch3/02_human_horse_classification.py
@@ -6,8 +6,34 @@ import zipfile
 url = "https://storage.googleapis.com/learning-datasets/validation-horse-or-human.zip"
 
 file_name = "validation-horse-or-human.zip"
-validation_dir = 'horse-or-human/validation/'
+training_dir = "horse-or-human/training/"
+validation_dir = "horse-or-human/validation/"
 urllib.request.urlretrieve(url, file_name)
 
-with zipfile.ZipFile(file_name, 'r') as zip_ref:
+with zipfile.ZipFile(file_name, "r") as zip_ref:
     zip_ref.extractall(validation_dir)
+    zip_ref.extractall(training_dir)
+
+
+# %%
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+# %%
+# 변환을 정의
+transform = transforms.Compose(
+    [
+        transforms.Resize((150, 150)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ]
+)
+
+# 데이터셋 로드
+train_dataset = datasets.ImageFolder(root=training_dir, transform=transform)
+val_dataset = datasets.ImageFolder(root=validation_dir, transform=transform)
+
+# 데이터 로더
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+# %%
