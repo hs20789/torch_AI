1b7a710900d1d74c93a311733f9ba6c3212601d6
HeonSu <hs20789@outlook.com>
2026-07-27 23:00:27 +0900

    ch3-3

--- chapters/ch3/02_human_horse_classification.py
+++ chapters/ch3/02_human_horse_classification.py
@@ -5,3 +5,9 @@ import zipfile
 # %%
 url = "https://storage.googleapis.com/learning-datasets/validation-horse-or-human.zip"
 
+file_name = "validation-horse-or-human.zip"
+validation_dir = 'horse-or-human/validation/'
+urllib.request.urlretrieve(url, file_name)
+
+with zipfile.ZipFile(file_name, 'r') as zip_ref:
+    zip_ref.extractall(validation_dir)
