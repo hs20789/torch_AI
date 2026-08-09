2df1e3d2dac37bd257330044a6a81b7dad4342a4
HeonSu <hs20789@outlook.com>
2026-08-10 00:23:54 +0900

    ch13-2

--- chapters/ch13/torchserve/model/model_train.py
+++ chapters/ch13/torchserve/model/model_train.py
@@ -3,15 +3,20 @@ import torch.nn as nn
 import torch.optim as optim
 from linear import SimpleLinearModel
 
+from pathlib import Path
+
+
 def train_model():
     model = SimpleLinearModel()
     criterion = nn.MSELoss()
     optimizer = optim.SGD(model.parameters(), lr=0.01)
 
+    # fmt: off
     xs = torch.tensor([[-1.0], [0.0], [1.0], [2.0], [3.0], [4.0]],
                       dtype=torch.float32)
     ys = torch.tensor([[-3.0], [-1.0], [1.0], [3.0], [5.0], [7.0]],
                       dtype=torch.float32)
+    # fmt: on
 
     for _ in range(500):
         optimizer.zero_grad()
@@ -22,5 +27,7 @@ def train_model():
 
     return model
 
+
 model = train_model()
-torch.save(model.state_dict(), "model.pth")
+model_path = Path(__file__).resolve().parent.parent / "model.pth"
+torch.save(model.state_dict(), model_path)
---
+++ chapters/ch13/torchserve/model_inference.py
@@ -0,0 +1,5 @@
+import torch
+import torch.nn as nn
+import os
+import sys
+from ts.torch_handler.base_handler import BaseHandler
